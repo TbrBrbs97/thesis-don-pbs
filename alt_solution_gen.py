c1bdb8a3a2173db8edfd97c8e228fcd0855d626d
TbrBrbs97 <tibor.barabas@student.kuleuven.be>
2022-03-26 19:08:43 +0100

    calc_best_insertion update

--- alt_solution_gen.py
+++ alt_solution_gen.py
@@ -25,19 +25,10 @@ def generate_initial_solution(requests_dict, current_service, nb_of_available_ve
         vehicles_schedule = init_fill_every_vehicle(requests_dict, nb_of_available_veh)
 
     else:
-        candidate_vehicle = find_best_vehicle_for_request_group(vehicles_schedule, request_group)
-        best_position_within_veh = find_best_position_within_vehicle(vehicles_schedule, request_group, candidate_vehicle)
-
-
-        # try to insert a request in the current position (find_closest_match?)
-        # make this a seperate function
-
-    # recursive part
-    # remove the request group from the request dictionairy
-    # return alt_create_initial_solution(requests_dict, current_service, nb_of_available_veh, vehicles_schedule)
-
-# if no spot in the current veh, or current veh > max_tour_length:
-    # return solution with new vehicle
+        candidate_position = find_best_position_for_request_group(vehicles_schedule, request_group)
+        # insert the request group in the candidate postion
+        # remove the request group from the request dictionairy
+        # return alt_create_initial_solution(requests_dict, current_service, nb_of_available_veh, vehicles_schedule)
 
 
 def init_fill_every_vehicle(request_dict, nb_of_available_veh, seed=True):
@@ -78,7 +69,7 @@ def pop_request(request_dictionairy):
     return random.choice(request_dictionairy[random_od_pair])
 
 
-def find_best_vehicle_for_request_group(vehicle_schedule, request_group, current_vehicle=1, best_vehicle_so_far=None):
+def find_best_position_for_request_group(vehicles_schedule, request_group, current_vehicle=1, best_pos_so_far=None):
     '''
     Function that returns the best possible position in the schedule for insertion,
     taking into account capacity constraints & the marginal cost of insertion
@@ -92,33 +83,35 @@ def find_best_vehicle_for_request_group(vehicle_schedule, request_group, current
     # - Second, you find the exact position within the chosen vehicle
 
     '''
-    if best_vehicle_so_far is None:
-        best_vehicle_so_far = 1
+
+    if best_pos_so_far is None:
+        best_pos_so_far = 1
 
     o, d = get_od_from_request_group(request_group)
     request_group_max_pt = get_max_pick_time(request_group)
 
     if current_vehicle > nb_of_required_ser:
-        return vehicle_schedule
+        return vehicles_schedule
 
-    if arc_in_vehicle(vehicle_schedule, current_vehicle, (o, d)) == (o, d) and \
-            remaining_capacity(vehicle_schedule, current_vehicle, (o, d)) > 0:
-        matching_score = calculate_matching_score(vehicle_schedule, current_vehicle, (o, d), request_group_max_pt)
-        if max(best_vehicle_so_far[1], matching_score) == matching_score:
-            best_vehicle_so_far = current_vehicle,  matching_score
+    if arc_in_vehicle(vehicles_schedule, current_vehicle, (o, d)) == (o, d):
+            # room_for_insertion_at_node(vehicle_schedule, current_vehicle, (o, d)) > 0:
+        matching_score = find_best_position_within_vehicle(vehicles_schedule, request_group, (o,d), current_vehicle)
+        if max(best_pos_so_far[1], matching_score) == matching_score:
+            best_pos_so_far = current_vehicle, matching_score
         # matching score is on the basis of the dep_time difference with the origin in this case
 
-    if arc_in_vehicle(vehicle_schedule, current_vehicle, (o, d)) == o and \
-            remaining_capacity(vehicle_schedule, current_vehicle, o) > 0:
-        matching_score = calculate_matching_score(vehicle_schedule, current_vehicle, o, request_group_max_pt)
-        if max(best_vehicle_so_far[1], matching_score) == matching_score:
-            best_vehicle_so_far = current_vehicle,  matching_score
+    if arc_in_vehicle(vehicles_schedule, current_vehicle, (o, d)) == o:
+        matching_score = find_best_position_within_vehicle(vehicles_schedule, request_group, o, current_vehicle)
+        if max(best_pos_so_far[1], matching_score) == matching_score:
+            best_pos_so_far = current_vehicle, matching_score
 
-    if arc_in_vehicle(vehicle_schedule, current_vehicle, (o, d)) == d and \
-            remaining_capacity(vehicle_schedule, current_vehicle, d) > 0:
-        matching_score = calculate_matching_score(vehicle_schedule, current_vehicle, d, request_group_max_pt)
-        if max(best_vehicle_so_far[1], matching_score) == matching_score:
-            best_vehicle_so_far = current_vehicle,  matching_score
+    if arc_in_vehicle(vehicles_schedule, current_vehicle, (o, d)) == d:
+        matching_score = find_best_position_within_vehicle(vehicles_schedule, request_group, d, current_vehicle)
+        if max(best_pos_so_far[1], matching_score) == matching_score:
+            best_pos_so_far = current_vehicle, matching_score
+
+    else: # in all other cases, you position the request group either in front of existing chain / behind it
+
 
     # if o in schedule + capacity left in between o and the next stop:
         # here
@@ -131,77 +124,177 @@ def find_best_vehicle_for_request_group(vehicle_schedule, request_group, current
         # if the dep time at o < the dep at the starting stop + driving time, add it before
         # in the case o > dep_t at the last stop + driving time, chain the arc in the end
 
-    current_vehicle += 1
-    return find_best_vehicle_for_request_group(vehicle_schedule, request_group, current_vehicle, best_vehicle_so_far)
+        current_vehicle += 1
+        return find_best_position_for_request_group(vehicles_schedule, request_group, current_vehicle, best_pos_so_far)
 
 
-def arc_in_vehicle(vehicle_schedule, current_vehicle, od_tup):
+def find_best_position_within_vehicle(vehicles_schedule, request_group, portion_matching_od, vehicle):
     '''
-    Returns the portion of the arc of 'od_tup' that is in the vehicle, as well as the number of times
-    this portions appears in the vehicle
+    Returns how a vehicles scores for accommodating a request group: this is based
+    on the difference between the departure time at a stop and the max pickup time of a request group.
+
+    >> check for capacity on this level!
+
+    Why not check for all possibilities? > i.e. not make it dependent whether or not o,d is (partly) matching in the schedule
+    '''
+
+    matching_score = 0
+    o, d = get_od_from_request_group(request_group)
+    request_group_max_pt = get_max_pick_time(request_group)
+
+    if portion_matching_od == (o, d):
+        # the function below will return the origin in the case of the arc o,d to be present
+        occ_of_node = get_all_occurrences_of_node(vehicles_schedule, vehicle, portion_matching_od)
+        best_matching_pos = calc_best_matching_position(vehicles_schedule, vehicle, request_group_max_pt, (o, d), occ_of_node, portion_matching_od)
+    elif portion_matching_od == o:
+        occ_of_node = get_all_occurrences_of_node(vehicles_schedule, vehicle, portion_matching_od)
+        best_matching_pos = calc_best_matching_position(vehicles_schedule, vehicle, request_group_max_pt, o, occ_of_node, portion_matching_od)
+    elif portion_matching_od == d:
+        return None
+    else: # check for the 'nearest stop' to either O or D
+        return None
+
+
+def get_prev_node(vehicles_schedule, vehicle, node):
+    nodes_in_vehicle = get_existing_nodes(vehicles_schedule, vehicle)
+    index_of_node = nodes_in_vehicle.index(node)
+    if index_of_node > 0:
+        return nodes_in_vehicle[index_of_node - 1]
+    else:
+        return None
+
+
+def get_next_node(vehicles_schedule, vehicle, node):
+    nodes_in_vehicle = get_existing_nodes(vehicles_schedule, vehicle)
+    index_of_node = nodes_in_vehicle.index(node)
+    if index_of_node < len(nodes_in_vehicle):
+        return nodes_in_vehicle[index_of_node + 1]
+    else:
+        return None
+
+
+def calc_best_matching_position(vehicles_schedule, vehicle, max_pt_rg, od_rg, occurrences, portion_matching_od):
+    '''
+    Given that there are multiple occurences of a node (e.g. ['2,0', '2,1', ...])
+    '''
+    # best_matching_node = previous_node, next_node, matching_score
+    best_matching_position = None
+
+    # if both the origin & dest. can be found as an arc, or if only the origin can be found as an arc
+    if portion_matching_od == od_rg or portion_matching_od == od_rg[0]:
+        for node in occurrences:
+            dep_time = get_departure_time_at_node(vehicles_schedule, vehicle, node)
+            delta_dep_time = abs(dep_time - max_pt_rg)
+
+            if best_matching_position is None or delta_dep_time < best_matching_position[1]\
+                    and room_for_insertion_at_node(vehicles_schedule, vehicle, node) > 0:
+                best_matching_position = node, delta_dep_time
+    # if only the destionation can be found as an arc
+    elif portion_matching_od == od_rg[1]:
+        for node in occurrences:
+            prev_node = get_prev_node(vehicles_schedule, vehicle, node)
+            if prev_node:
+                dep_time = get_departure_time_at_node(vehicles_schedule, vehicle, prev_node)
+                delta_dep_time = abs(dep_time - max_pt_rg)
+
+                if best_matching_position is None or delta_dep_time < best_matching_position[1] \
+                        and room_for_insertion_at_node(vehicles_schedule, vehicle, node) > 0:
+                    best_matching_position = prev_node, delta_dep_time
+            else: # meaning that if the destination happens to be the first stop in the schedule
+                dep_time = get_departure_time_at_node(vehicles_schedule, vehicle, node)
+                delta_dep_time = abs(dep_time - max_pt_rg)
+
+                if best_matching_position is None or delta_dep_time < best_matching_position[1] \
+                        and room_for_insertion_at_node(vehicles_schedule, vehicle, node) > 0:
+                    best_matching_position = prev_node, delta_dep_time
+
+    return best_matching_position
+
+
+def arc_in_vehicle(vehicles_schedule, current_vehicle, od_tup):
+    '''
+    Returns the portion of the arc of 'od_tup' that is in the vehicle (i.e. the portion_matching_od),
+    as well as the number of times this portions appears in the vehicle
     '''
     o, d = od_tup
 
-    if any([int(node[0]) == o for node in vehicle_schedule[current_vehicle]]) and \
-            any([int(node[0]) == d for node in vehicle_schedule[current_vehicle]]):
-        return o, d  #, len([int(node_o[0]) == o and int(node_d[0]) == d for node_o in vehicle_schedule[current_vehicle] for node_d in vehicle_schedule[current_vehicle]])
-    elif any([int(node[0]) == o for node in vehicle_schedule[current_vehicle]]):
-        return o  #, len([int(node[0]) == o for node in vehicle_schedule[current_vehicle]])
-    elif any([int(node[0]) == d for node in vehicle_schedule[current_vehicle]]):
-        return d  #, len([int(node[0]) == d for node in vehicle_schedule[current_vehicle]])
+    if any([int(node[0]) == o for node in vehicles_schedule[current_vehicle]]) and \
+            any([int(node[0]) == d for node in vehicles_schedule[current_vehicle]]):
+        return o, d
+    elif any([int(node[0]) == o for node in vehicles_schedule[current_vehicle]]):
+        return o
+    elif any([int(node[0]) == d for node in vehicles_schedule[current_vehicle]]):
+        return d
     else:
         return False
 
 
-def find_best_position_within_vehicle(vehicle_schedule, request_group, vehicle):
-    return None
-
-
-def insert_request_group(vehicles_schedule, request_group, vehicle, position):
-    return None
-
-
-def get_existing_arcs(vehicles_schedule, service):
+def get_existing_arcs(vehicles_schedule, vehicle):
     '''
     Function that returns a list of the already present arcs in the vehicle schedule, created
     by the pick-up and drop-off of passengers.
     '''
 
-    nodes = list(vehicles_schedule[service].keys())
+    nodes = list(vehicles_schedule[vehicle].keys())
     return [(nodes[i], nodes[i+1]) for i in range(len(nodes)-1)]
 
 
-def get_all_occurences_of_node(vehicles_schedule, service, portion_of_od_in_sched):
+def get_existing_nodes(vehicles_schedule, vehicle):
+    '''
+    Function that returns the list of existing nodes/stops in a vehicle schedule.
+    '''
+    return list(vehicles_schedule[vehicle])
+
+
+def get_all_occurrences_of_node(vehicles_schedule, service, portion_of_od_in_sched):
     '''
     Function that returns a list of all occurences of a certain stop
-    in the schedule of a vehicle
+    in the schedule of a vehicle. We only need to know if either the origin or the destination is within the vehicle.
     '''
-    if not portion_of_od_in_sched is tuple:
-        return [n for n in vehicles_schedule[service] if int(n[0]) == portion_of_od_in_sched]
-    else:
-        # otherwise we want to know how often the origin is in the schedule
+
+    if portion_of_od_in_sched is tuple:
         return [n for n in vehicles_schedule[service] if int(n[0]) == portion_of_od_in_sched[0]]
+    else:
+        return [n for n in vehicles_schedule[service] if int(n[0]) == portion_of_od_in_sched]
 
 
-def remaining_capacity(vehicles_schedule, vehicle, arc):
+def room_for_insertion_at_node(vehicles_schedule, vehicle, node):
     '''
-    Returns the available capacity on a given arc,
-    taken into account the number of available seates parameter
+    Returns the available capacity for insertion at a node 'node'. The function essentially diminishes the capacity
+    per vehicle (which is a parameter) with the amount of request which obey the following criteria:
+    1) The requests already boarding the vehicle at 'node';
+    2) The requests boarding at any node before 'node', and also having a drop-off node after 'node'.
+
     '''
-    # this is defined on a link-basis between two nodes (e.g. between 1 and 2)
-    # The occupied load between o and d are all passengers with origin: 1 and destination != 1
-    occupied_spots = set(())
 
-    # check for all stops beyond the starting position until the ending
-    for a in range(arc[0], arc[1]):
-        if len(vehicles_schedule[vehicle][a]) != 0:
-            x = {j for i in vehicles_schedule[vehicle][a][1:] for j in i}
-            occupied_spots = occupied_spots.union(x)
+    pax_boarding_at_current_stop = sum([len(request_group) for request_group in vehicles_schedule[vehicle][node][1:]])
+    pax_from_prev_stops = count_transit_pax_over_node(vehicles_schedule, vehicle, node)
 
-    return cap_per_veh - len(occupied_spots)
+    return cap_per_veh - (pax_boarding_at_current_stop + pax_from_prev_stops)
 
 
-def get_departure_time(vehicles_schedule, vehicle, node):
+def count_transit_pax_over_node(vehicles_schedule, vehicle, node):
+    '''
+    Function that counts the nb of passengers who enter the vehicle before 'node',
+    and remain in the vehicle until after 'node'.
+    '''
+    all_nodes = get_existing_nodes(vehicles_schedule, vehicle)
+    index_curr_node = all_nodes.index(node)
+    all_previous_nodes = all_nodes[:index_curr_node]
+
+    transit_pax = 0
+
+    for node in all_previous_nodes:
+        # TODO
+        # this will not work! in the case there are multiple instances of the destination >> you have to know where people get off
+        # >> maybe build a dictionairy with keys = request, value = [vehicle, pick-up node, dep_time, drop_off node, arr_time]
+        # this will probably also be easier to refer to later! >> and calculate IVT & WT for later
+        transit_pax += sum([len(request_group) for request_group in vehicles_schedule[vehicle][node][1:] if get_max_pick_time(request_group)])
+
+    return transit_pax
+
+
+def get_departure_time_at_node(vehicles_schedule, vehicle, node):
     return vehicles_schedule[vehicle][node][0]
 
 
@@ -215,24 +308,12 @@ def insert_stop_in_vehicle(vehicle_schedule, vehicle, new_stop, previous_stop = 
 
     see: https://stackoverflow.com/questions/44390818/how-to-insert-key-value-pair-into-dictionary-at-a-specified-position
     '''
+    return None
 
 
-def calculate_matching_score(vehicles_schedule, vehicle, request_group, portion_of_od_in_sched):
-    '''
-    Returns how a vehicles scores for accomodating a request group: this is based
-    on the difference between the departure time at a stop and the max pickup time of a request group.
-    '''
+def insert_request_group(vehicles_schedule, request_group, vehicle, position):
+    return None
 
-    matching_score = 0
-    o, d = get_od_from_request_group(request_group)
-    request_group_max_pt = get_max_pick_time(request_group)
 
-    if type(portion_of_od_in_sched) == (o, d):
-        occurences = get_all_occurences_of_node(vehicles_schedule, vehicle, portion_of_od_in_sched)
-        best_matching_node = None
-        for node in occurences:
-            dep_time = get_departure_time(vehicles_schedule, vehicle, node)
-            delta_dep_time = abs(dep_time - request_group_max_pt)
-            if best_matching_node is None or delta_dep_time < best_matching_node[1]:
-                best_matching_node = node, delta_dep_time
+
 
--- testfile_initial_solution.py
+++ testfile_initial_solution.py
@@ -8,7 +8,8 @@ import solution_evaluation as se
 import vehicle_generation as vg
 import solution_visualisation as sv
 
-from alt_solution_gen import generate_initial_solution, init_fill_every_vehicle, pop_request, get_existing_arcs, arc_in_vehicle
+from alt_solution_gen import generate_initial_solution, init_fill_every_vehicle, pop_request, \
+    get_existing_arcs, get_existing_nodes, arc_in_vehicle, get_all_occurrences_of_node, get_next_node, get_prev_node
 
 from parameters import network, lambdapeak, mupeak, demand_scenario, peak_duration, \
     req_max_cluster_time, cap_per_veh, max_services_per_vehicle, cost_matrix, grouped_requests, nb_of_required_ser
@@ -17,45 +18,14 @@ initial_solution = init_fill_every_vehicle(grouped_requests, nb_of_required_ser)
 for i in initial_solution:
      print('veh: ', i, ', stops: ', initial_solution[i])
 
-print(arc_in_vehicle(initial_solution, 1, (2, 3)))
+# portion_matching_od = arc_in_vehicle(initial_solution, 1, (2, 3))
+# print(portion_matching_od)
+# all_occ_of_node = get_all_occurrences_of_node(initial_solution, 1, 3)
+print(get_existing_nodes(initial_solution, 1))
+print(get_next_node(initial_solution, 1, '1,0'))
 
-# random_request = pop_request(grouped_requests)
-# print(random_request)
 
 
-# vehicles_schedules = sg.services_to_vehicles(initial, par.max_services_per_vehicle)
-# #print(grouped_requests)
-# print(vehicles_schedules[(3,1)])
-#
-# waiting_time_dict = se.calc_waiting_time(vehicles_schedules)
-# #print(waiting_time_dict[(2, 2)])
-# inveh_time_dict = se.calc_in_vehicle_time(vehicles_schedules)
-# #print(inveh_time_dict[(2, 2)])
-# total_tt_dict = se.calculate_ttt(inveh_time_dict, waiting_time_dict)
-# #print(total_tt_dict)
-#
-# wt_stops = se.sum_total_tt(waiting_time_dict, level='stop')
-# ivt_stops = se.sum_total_tt(inveh_time_dict, level='stop')
-# sum_stops = se.sum_total_tt(total_tt_dict, level='stop')
-# #print('sum stops= ', sum_stops)
-#
-# sum_vehicle = se.sum_total_tt(total_tt_dict, level='vehicle')
-# #print('sum vehicles= ', sum_vehicle)
-#
-# sum_total = se.sum_total_tt(total_tt_dict, level='total')
-# print('sum total= ', sum_total)
-# print('average travel time per passenger = ', sum_total/len(list_all_requests))
-#
-# occ = se.calc_occupancy_rate(vehicles_schedules, par.cap_per_veh)
-# #print(occ)
-#
-# # Convert to DF and export to excel:
-#
-# df_solution = sv.convert_to_dataframe(vehicles_schedules)
-# df_wt = sv.convert_to_dataframe(wt_stops)
-# df_ivt = sv.convert_to_dataframe(ivt_stops)
-# df_occ = sv.convert_to_dataframe(occ)
-#
 # # maybe you need one column extra (abboard_pax3) ! Depends...
 # col_names = ['dep_time', 'abboard_pax1', 'abboard_pax2', 'sum_wt', 'sum_ivt', 'veh_occ']
 # df_all = pd.concat([df_solution, df_wt, df_ivt, df_occ], axis=1)
