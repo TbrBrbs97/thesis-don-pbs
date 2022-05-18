a83794a3273d3a174ed69bcdce893f4972816bff
TbrBrbs97 <tibor.barabas@student.kuleuven.be>
2022-05-18 22:54:08 +0200

    test waiting time, travel time and direction evaluation

--- testfile.py
+++ testfile.py
@@ -19,7 +19,7 @@ from solution_construct import generate_initial_solution, static_optimization, \
 
 from solution_evaluation import calc_request_group_waiting_time, calc_request_group_invehicle_time, \
     get_objective_function_val, generate_waiting_time_dict, generate_in_vehicle_time_dict, \
-    generate_total_travel_time_dict, select_most_costly_request_groups, calc_total_vehicle_kilometers
+    generate_total_travel_time_dict, select_most_costly_request_groups, calc_total_vehicle_kilometers, sum_total_travel_time
 
 from parameters import network, lambdapeak, mupeak, demand_scenario, peak_duration, \
     req_max_cluster_time, cap_per_veh, nb_of_available_vehicles, \
@@ -64,28 +64,37 @@ for i in initial_solution:
 print('objective func: ', get_objective_function_val(initial_solution, relative=False))
 print(count_total_assigned_requests(initial_solution))
 
+waiting_time = generate_waiting_time_dict(initial_solution)
+in_veh_time = generate_in_vehicle_time_dict(initial_solution)
+
+print('avg. waiting time: ', sum_total_travel_time(waiting_time))
+print('avg. in-vehicle time: ', sum_total_travel_time(in_veh_time))
+
+print('city requests avg. TT: ', get_objective_function_val(initial_solution, relative=True, direction='city'))
+print('terminal requests avg. TT: ', get_objective_function_val(initial_solution, relative=True, direction='terminal'))
+
 ## OPTIMIZED STATIC SOLUTION
 
-optimized_solution, best_iteration = static_optimization(network, initial_solution,
-                                                         required_requests_per_it=steep_descent_intensity,
-                                                         time_limit=opt_time_lim, capacity=cap_per_veh, depot=depot)
-for i in optimized_solution:
-    print('veh ', i, ': ', optimized_solution[i])
-print('overall objective function: ', get_objective_function_val(optimized_solution, relative=False),
-      'at iteration: ', best_iteration)
-print('avg. travel time per passenger: ', get_objective_function_val(optimized_solution, relative=True))
-
-
-## DYNAMIC SOLUTION
-
-if degree_of_dynamism > 0.0:
-    dynamic_initial_solution = generate_dynamic_solution(network, optimized_solution, all_dynamic_requests,
-                                                         lead_time=lead_time, peak_hour_duration=peak_duration,
-                                                         capacity=cap_per_veh, depot=depot)
-    for i in dynamic_initial_solution:
-        print('veh ', i, ': ', dynamic_initial_solution[i])
-    print('objective func: ', get_objective_function_val(dynamic_initial_solution, relative=False))
-    print('avg. travel time per passenger: ', get_objective_function_val(dynamic_initial_solution, relative=True))
+# optimized_solution, best_iteration = static_optimization(network, initial_solution,
+#                                                          required_requests_per_it=steep_descent_intensity,
+#                                                          time_limit=opt_time_lim, capacity=cap_per_veh, depot=depot)
+# for i in optimized_solution:
+#     print('veh ', i, ': ', optimized_solution[i])
+# print('overall objective function: ', get_objective_function_val(optimized_solution, relative=False),
+#       'at iteration: ', best_iteration)
+# print('avg. travel time per passenger: ', get_objective_function_val(optimized_solution, relative=True))
+#
+#
+# ## DYNAMIC SOLUTION
+#
+# if degree_of_dynamism > 0.0:
+#     dynamic_initial_solution = generate_dynamic_solution(network, optimized_solution, all_dynamic_requests,
+#                                                          lead_time=lead_time, peak_hour_duration=peak_duration,
+#                                                          capacity=cap_per_veh, depot=depot)
+#     for i in dynamic_initial_solution:
+#         print('veh ', i, ': ', dynamic_initial_solution[i])
+#     print('objective func: ', get_objective_function_val(dynamic_initial_solution, relative=False))
+#     print('avg. travel time per passenger: ', get_objective_function_val(dynamic_initial_solution, relative=True))
 
 
 # cProfiler
