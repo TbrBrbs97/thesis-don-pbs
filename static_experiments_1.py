6b22e00d7dd1123be26c220f8156b67277107840
Tibor-ASUS <tiborbrbs97@gmail.com>
2022-05-21 13:24:59 +0200

    se_1

--- static_experiments_1.py
+++ static_experiments_1.py
@@ -1,13 +1,7 @@
 import pickle
 
-<<<<<<< HEAD
-from settings import v_mean, demand_scenario, demand_subscenario, req_max_cluster_time, peak_duration, \
-    degree_of_dynamism, lead_time, nb_of_available_vehicles, steep_descent_intensity, opt_time_lim
-=======
-from parameters import v_mean, demand_scenario, demand_subscenario, req_max_cluster_time, \
-    degree_of_dynamism, nb_of_available_vehicles, steep_descent_intensity, opt_time_lim
->>>>>>> a83794a3273d3a174ed69bcdce893f4972816bff
-
+from settings import v_mean, demand_scenario, demand_subscenario, degree_of_dynamism, \
+    lead_time, nb_of_available_vehicles, steep_descent_intensity
 from network_generation import import_network, generate_cost_matrix, \
     get_network_boundaries, generate_distance_matrix
 
@@ -20,7 +14,7 @@ from solution_construct import generate_initial_solution, static_optimization
 # PARAMETERS
 network_sizes = ['small', 'medium', 'large', 'real']
 network_variants = ['half', 'more']
-nb_of_samples = 4
+nb_of_samples = 2
 
 for size in network_sizes:
     for variant in network_variants:
@@ -33,17 +27,23 @@ for size in network_sizes:
 
             if network_size == 'small':
                 peak_duration = 20  # min.
+                opt_time_limit = 60
             elif network_size == 'medium':
                 peak_duration = 40
+                opt_time_limit = 120
             elif network_size == 'large':
                 peak_duration = 60
+                opt_time_limit = 240
             else:
                 peak_duration = 120
+                opt_time_limit = 480
 
             network = import_network(network_size, network_variant)
             cost_matrix = generate_cost_matrix(network, v_mean)
             distance_matrix = generate_distance_matrix(network)
             network_dim = get_network_boundaries(network)
+
+            req_max_cluster_time = peak_duration / 4
 
             lambdapeak = get_scenario_mean_demand('city', network_size, scen=demand_scenario,
                                                   subscen=demand_subscenario, peak=1)
@@ -75,7 +75,7 @@ for size in network_sizes:
             # OPTIMIZED SOLUTION
             optimized_solution, best_iteration = static_optimization(network, initial_solution,
                                                                      required_requests_per_it=steep_descent_intensity,
-                                                                     time_limit=opt_time_lim)
+                                                                     time_limit=opt_time_limit)
             opt_name = 'Results/SE_1/' + network_size + '_' + network_variant + '_' + str(sample) + '_opt'
             file_to_write_2 = open(opt_name, "wb")
             pickle.dump(optimized_solution, file_to_write_2)
